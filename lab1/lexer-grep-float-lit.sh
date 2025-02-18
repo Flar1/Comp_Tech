e4f10b6c9a3aa7903ad6f71b17c766997bfb1116
Flar1 <Flar1@example.com>
2025-02-19 00:54:32 +0700

    fix code

--- lab1/lexer-grep-float-lit.sh
+++ lab1/lexer-grep-float-lit.sh
@@ -7,6 +7,6 @@ REGEXP2="[0-9']+[\.]"
 REGEXP3="[0-9']*[\.][0-9']+[eE][+-]?[0-9']*[lf]?"
 REGEXP4="0[xX][0-9a-fA-F']*\.[0-9a-fA-F']*[pP][0-9a-fA-F']+"
 
-FLOAT_REGEXP="$REGEXP1|$REGEXP2|$REGEXP3|$REGEXP3"
+FLOAT_REGEXP="$REGEXP1|$REGEXP2|$REGEXP3|$REGEXP4"
 
 grep -E -o "$FLOAT_REGEXP" "$FILE"
